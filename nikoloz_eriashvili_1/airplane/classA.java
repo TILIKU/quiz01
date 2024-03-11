498c6859c0d5448e43910fc66dd72dc5c3aa332d
TILIKU <85792875+TILIKU@users.noreply.github.com>
2024-03-11 11:54:17 +0400

    think i did everything right
    instructions were kind of unclear but whatever we up

---
+++ nikoloz_eriashvili_1/airplane/classA.java
@@ -0,0 +1,10 @@
+package airplane;
+
+public class classA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ nikoloz_eriashvili_1/ocean/Assignment03.java
@@ -0,0 +1,14 @@
+package ocean;
+
+
+/**
+ * Find the file "hidden.txt" and print its path.
+ * The file should be found yourself without java code.
+ * 0.5 points
+ */
+public class Assignment03 {
+	public static void main (String[] args) {
+	 System.out.println("C:\\Users\\TILIKU\\Desktop\\oop2024spring-main\\oop2024spring-main\\quizes\\q1\\nikoloz_eriashvili_1\\ocean\\truck\\highlighter\\hidden.txt");
+
+	}
+}
